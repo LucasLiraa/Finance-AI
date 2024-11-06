455d279b210f84a592c6714677d10814e59767cd
Lucas Lira <lucas.palira@gmail.com>
2024-11-05 21:21:08 -0300

    Primeiro commit

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+const Home = () => {
+  return;
+  <div className="flex w-screen items-center justify-center">
+    <h1 className="p-5 text-red-500">Hello World</h1>
+  </div>;
+};
+
+export default Home;
